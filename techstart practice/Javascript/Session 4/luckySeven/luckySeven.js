39bd866939e3dc8d4b2497299d688406f611b9ba
kimberly-hernandez <60126633+kimberly-hernandez@users.noreply.github.com>
2022-07-25 18:57:05 -0700

    Add files via upload

---
+++ techstart practice/Javascript/Session 4/luckySeven/luckySeven.js
@@ -0,0 +1,12 @@
+const numberFun = document.forms["numberFun"];
+const num1 = document.getElementById("num1");
+
+function myFunction(){
+    if(num1 >0 ){
+  
+        console.log("continue")
+    }
+    else{
+        console.log("enter value greater than $0")
+    }
+}
